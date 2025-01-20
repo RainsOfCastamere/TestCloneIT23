9ba498efa8083b708e7074e6d3405fb51f42f4c9
RainsOfCastamere <thomas.riegler@gmail.com>
2025-01-20 16:26:31 +0100

    added ?

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,5 +10,5 @@ Console.WriteLine("WIe stehts?");
 
 
 
-Console.WriteLine("I bins da Moritz und mir geht´s guad wie rennts mid de Noten");
+Console.WriteLine("I bins da Moritz und mir geht´s guad wie rennts mid de Noten?");
 Console.WriteLine("");
