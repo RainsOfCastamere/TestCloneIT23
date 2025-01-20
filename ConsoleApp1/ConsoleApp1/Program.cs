ca47beb3865327ea762007fc0ccd07070f9f2472
MoritzHintermair <moritzhin09@gmail.com>
2025-01-20 16:14:24 +0100

    Note

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,5 +11,5 @@ Console.WriteLine("WIe stehts?");
 
 
 
-Console.WriteLine("I bins da Moritz und i bin a bisl dumm");
+Console.WriteLine("I bins da Moritz und mir geht´s guad wie rennts mid de Noten");
 Console.WriteLine("");
